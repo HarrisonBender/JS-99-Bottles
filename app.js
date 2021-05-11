eaae6bd65ee3d5ea287a1ffc57e2122802cbac6a
HarrisonBender <83611772+HarrisonBender@users.noreply.github.com>
2021-05-11 16:44:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+let friends = ["John ", "George ", "Paul ", "Ringo ", "Bernard "];
+
+console.log(friends[0]);
+
+var num = 99;
+
+while (num >= 1) {
+
+if (num === 2) {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[0] + "strikes one out, clears it all out, "
++ (num-1) + " line of code in the file, ")
+}else if (num === 1) {
+console.log(num + " line of code in the file, "
++ num + " line of code;" + " " + friends[0] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file. ")
+
+} else {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[0] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file, ")
+
+}
+
+num --;
+
+}
+
+console.log(friends[1]);
+
+var num = 99;
+
+while (num >= 1) {
+
+if (num === 2) {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[1] + "strikes one out, clears it all out, "
++ (num-1) + " line of code in the file, ")
+}else if (num === 1) {
+console.log(num + " line of code in the file, "
++ num + " line of code;" + " " + friends[1] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file. ")
+
+} else {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[1] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file, ")
+
+}
+
+num --;
+
+}
+
+
+console.log(friends[2]);
+
+var num = 99;
+
+while (num >= 1) {
+
+if (num === 2) {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[2] + "strikes one out, clears it all out, "
++ (num-1) + " line of code in the file, ")
+}else if (num === 1) {
+console.log(num + " line of code in the file, "
++ num + " line of code;" + " " + friends[2] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file. ")
+
+} else {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[2] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file, ")
+
+}
+
+num --;
+
+}
+
+console.log(friends[3]);
+
+var num = 99;
+
+while (num >= 1) {
+
+if (num === 2) {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[3] + "strikes one out, clears it all out, "
++ (num-1) + " line of code in the file, ")
+}else if (num === 1) {
+console.log(num + " line of code in the file, "
++ num + " line of code;" + " " + friends[3] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file. ")
+
+} else {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[3] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file, ")
+
+}
+
+num --;
+
+}
+
+console.log(friends[4]);
+
+var num = 99;
+
+while (num >= 1) {
+
+if (num === 2) {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[4] + "strikes one out, clears it all out, "
++ (num-1) + " line of code in the file, ")
+}else if (num === 1) {
+console.log(num + " line of code in the file, "
++ num + " line of code;" + " " + friends[4] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file. ")
+
+} else {
+console.log(num + " lines of code in the file, "
++ num + " lines of code;" + " " + friends[4] + "strikes one out, clears it all out, "
++ (num-1) + " lines of code in the file, ")
+
+}
+
+num --;
+
+}
